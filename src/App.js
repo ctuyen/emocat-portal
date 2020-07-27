0945d3e1c8ea0b045a1c9c8bbfc743537e4bd2b8
TuyenLe <congtuyen598@gmail.com>
2020-07-28 03:12:13 +0700

    add statistics page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Header from "./Header";
 import EmoFace from "./EmoFace";
+import Statistics from "./Statistics";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <EmoFace/>
+      <Router>
+        <Switch>
+          <Route path={'/statistics'}><Statistics/></Route>
+          <Route path={'/demo'}><EmoFace/></Route>
+          <Route path={'/'}><EmoFace/></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
